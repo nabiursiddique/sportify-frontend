518787a45ac26f7de586086302b45ad937df655e
Nabiur siddique <nabiursiddique01@gmail.com>
2024-07-09 11:24:04 +0600

    shadcn setup is completed

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Sportify is Loading</h1>
+      <Button>The Shadcn Button</Button>
+    </div>
+  );
+};
+
+export default App;
