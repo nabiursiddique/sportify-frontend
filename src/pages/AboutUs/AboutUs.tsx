f64391397dc638ffebe87cf5b36c6948386bc045
Nabiur siddique <nabiursiddique01@gmail.com>
2024-07-12 20:57:36 +0600

    our team section is completed

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,11 +1,13 @@
 import AboutCompany from "@/components/AboutUsComponents/AboutCompany";
 import AboutUsHeader from "@/components/AboutUsComponents/AboutUsHeader";
+import OurTeam from "@/components/AboutUsComponents/OurTeam";
 
 const AboutUs = () => {
   return (
     <div>
       <AboutUsHeader />
       <AboutCompany />
+      <OurTeam />
     </div>
   );
 };
